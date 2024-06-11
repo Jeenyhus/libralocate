7b27eb93b0bde26f0b88706cf561619c4a5c79cb
Jeenyhus <mweembadabwitso@gmail.com>
2024-06-11 14:43:42 +0200

    registered the models in the admin.py file

--- libra_locate/libra/admin.py
+++ libra_locate/libra/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Book, Store, BookStore
 
-# Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    """Admin class for Book model."""
+    list_display = ('title', 'author', 'isbn', 'published_date')
+    search_fields = ('title', 'author', 'isbn')
+
+@admin.register(Store)
+class StoreAdmin(admin.ModelAdmin):
+    """
+    Admin class for Store model.
+    """
+    list_display = ('name', 'url', 'address', 'phone')
+    search_fields = ('name', 'url')
+
+@admin.register(BookStore)
+class BookStoreAdmin(admin.ModelAdmin):
+    """
+    Admin class for BookStore model.
+    """
+    list_display = ('book', 'store', 'price', 'availability')
+    search_fields = ('book__title', 'store__name')
+    list_filter = ('availability',)
