0bb0f6fd123057e51c0f958da5407e7571909d09
Jeenyhus <mweembadabwitso@gmail.com>
2024-06-12 06:07:31 +0200

    this serves files to render the frontend

--- libra_locate/libra/views.py
+++ libra_locate/libra/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
 def index(request):
-    return render(request, 'libra/index.html')
+    """
+    Renders the main Vue.js application.
+    """
+    return render(request, 'index.html')
